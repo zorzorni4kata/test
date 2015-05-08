fcff148054422fef56bfc07324d66074888b3853
Tonislav Atanasov <tonislav.atanasov@gmail.com>
2015-05-08 10:34:42 +0300

    Update GiftForMyNiggaz-SortedSubsets.cs
    - Solution to Sorted Subsets from Advanced C# Homework 1 using binary logic
    - Fixed minor issue where the program didn't calculate all possible combinations
    - Added further comments for easier understanding

--- GiftForMyNiggaz-SortedSubsets.cs
+++ GiftForMyNiggaz-SortedSubsets.cs
@@ -22,22 +22,30 @@ class SortedSubsets
         // calculate possible combinations and iterate through
         int combinations = (int)Math.Pow(2, numbers.Length);
 
-        for (int i = 1; i < combinations; i++)
+        for (int i = 1; i <= combinations; i++)
         {
             int currentSum = 0;
             List<int> currentSubset = new List<int>();
 
+            /* check the binary representation of the current combination (1 to 2^n) 
+             * sum the numbers at indexes where the bit value is 1
+             * e.g. 5 = 101 in binary so our 5th combination is sum of numbers[0] + numbers[2]
+             */
             for (int j = 0; j < 32; j++)
             {
                 if (((i >> j) & 1) == 1)
                 {
                     currentSum += numbers[j];
+                    /* we need a list of all numbers in the subset for sorting purposes 
+                     * we can also use a string to keep the current subset but this way
+                     * sorting the output is much more difficult */
                     currentSubset.Add(numbers[j]);
                 }
             }
 
-            // check if the current sum equals the sum we're looking for
-            // if yes -> sort the elements of the list and add it to results
+            /* check if the current sum equals the sum we're looking for
+             * if yes -> sort the elements of the list and add it to results
+             * sort the subset list so numbers are in increasing order, this will help later on */
             if (currentSum == sum)
             {
                 currentSubset.Sort();
@@ -52,9 +60,12 @@ class SortedSubsets
             return;
         }
 
-        // sort the output by subset length and then by value of the first number
+        /* because we already sorted the subsets in increasing order the smallest numbers in the subsets are first 
+         * and we can now sort the output by subset length and then by value of the first number
+         */
         var output = results.OrderBy(subset => subset.Count).ThenBy(subset => subset.First());
         
+        // now print we print the output and hope for the best
         foreach (var subset in output)
         {
             foreach (var number in subset)
